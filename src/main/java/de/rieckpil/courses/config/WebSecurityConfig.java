2cb40b425af8fe02da238223a2415b8e610eafc7
Mike Smithson <mikesmithso@gmail.com>
2024-02-29 22:38:40 -0500

    format files; update deps

--- src/main/java/de/rieckpil/courses/config/WebSecurityConfig.java
+++ src/main/java/de/rieckpil/courses/config/WebSecurityConfig.java
@@ -1,8 +1,8 @@
 package de.rieckpil.courses.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.boot.actuate.health.HealthEndpoint;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -17,21 +17,30 @@ public class WebSecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
     httpSecurity
-      .authorizeHttpRequests(authorize -> authorize
-        .requestMatchers(HttpMethod.GET, "/api/books").permitAll()
-        .requestMatchers(HttpMethod.GET, "/api/books/reviews").permitAll()
-        .requestMatchers("/api/**").authenticated()
-        .requestMatchers(EndpointRequest.to(HealthEndpoint.class)).permitAll()
-        .anyRequest().permitAll()
-      )
-      .sessionManagement()
-      .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-      .and()
-      .cors()
-      .and()
-      .csrf().disable()
-      .oauth2ResourceServer(oauth2 ->
-        oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(new CustomAuthenticationConverter())));
+        .authorizeHttpRequests(
+            authorize ->
+                authorize
+                    .requestMatchers(HttpMethod.GET, "/api/books")
+                    .permitAll()
+                    .requestMatchers(HttpMethod.GET, "/api/books/reviews")
+                    .permitAll()
+                    .requestMatchers("/api/**")
+                    .authenticated()
+                    .requestMatchers(EndpointRequest.to(HealthEndpoint.class))
+                    .permitAll()
+                    .anyRequest()
+                    .permitAll())
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        .cors()
+        .and()
+        .csrf()
+        .disable()
+        .oauth2ResourceServer(
+            oauth2 ->
+                oauth2.jwt(
+                    jwt -> jwt.jwtAuthenticationConverter(new CustomAuthenticationConverter())));
 
     return httpSecurity.build();
   }
