fe5e571411e8c484c1315dd300d23980938c6fe0
Mike Smithson <mikesmithso@gmail.com>
2024-03-02 17:59:53 -0500

    using json assert and json path matchers

--- src/test/java/de/rieckpil/courses/book/JsonTest.java
+++ src/test/java/de/rieckpil/courses/book/JsonTest.java
@@ -1,8 +1,13 @@
 package de.rieckpil.courses.book;
 
+import com.jayway.jsonpath.JsonPath;
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class JsonTest {
 
@@ -22,8 +27,9 @@ public class JsonTest {
     String result = """
         {"age":"42", "name": "duke", "tags":["java", "jdk"], "orders": [42, 42, 16]};
       """;
-    JSONAssert.assertEquals("""
-      {"orders": [42, 42, 16]}
-      """, result, false);
+    assertThat(JsonPath.parse(result).read("$.age", Integer.class)).isEqualTo(42);
+    assertThat(JsonPath.parse(result).read("$.name", String.class)).isEqualTo("duke");
+    assertThat(JsonPath.parse(result).read("$.tags", List.class)).isEqualTo(List.of("java", "jdk"));
+    assertThat(JsonPath.parse(result).read("$.orders", List.class)).isEqualTo(List.of(42, 42, 16));
   }
 }
