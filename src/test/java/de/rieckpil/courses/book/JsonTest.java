f3e37bfcbf7b450afd31cd7ceb7b93219070200f
Mike Smithson <mikesmithso@gmail.com>
2024-03-02 17:38:24 -0500

    json assert tests

--- src/test/java/de/rieckpil/courses/book/JsonTest.java
+++ src/test/java/de/rieckpil/courses/book/JsonTest.java
@@ -2,17 +2,28 @@ package de.rieckpil.courses.book;
 
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 
 public class JsonTest {
 
   @Test
   void testWithJSONAssert() throws JSONException {
-    String result = "{\"name\": \"duke\", \"age\":\"42\", \"hobbies\": [\"soccer\", \"java\"]}";
+    String result = """
+      {"name": "duke", "age":"42", "hobbies": ["soccer", "java"]}
+      """;
+
+    JSONAssert.assertEquals("""
+      { "hobbies": ["soccer", "java"]}
+       """, result, false);
   }
 
   @Test
-  void testWithJsonPath() {
-    String result =
-        "{\"age\":\"42\", \"name\": \"duke\", \"tags\":[\"java\", \"jdk\"], \"orders\": [42, 42, 16]}";
+  void testWithJsonPath() throws JSONException {
+    String result = """
+        {"age":"42", "name": "duke", "tags":["java", "jdk"], "orders": [42, 42, 16]};
+      """;
+    JSONAssert.assertEquals("""
+      {"orders": [42, 42, 16]}
+      """, result, false);
   }
 }
