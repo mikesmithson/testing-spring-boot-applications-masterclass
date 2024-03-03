8be74dbb5cfbe6239a296e3cad1fa3062f452639
Mike Smithson <mikesmithso@gmail.com>
2024-03-03 13:55:45 -0500

    Postgres containers test with and without reuse of the Postgres container

--- src/test/java/de/rieckpil/courses/book/review/ReviewRepositoryReuseTestContainersTest.java
+++ src/test/java/de/rieckpil/courses/book/review/ReviewRepositoryReuseTestContainersTest.java
@@ -3,6 +3,7 @@ package de.rieckpil.courses.book.review;
 import de.rieckpil.courses.book.management.Book;
 import de.rieckpil.courses.book.management.User;
 import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +11,9 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.security.core.parameters.P;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -24,21 +25,23 @@ import java.time.LocalDateTime;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-@Testcontainers
 @AutoConfigureTestDatabase(replace = Replace.NONE)
-class ReviewRepositoryTestContainersTest {
-
-  @Container
-  private static PostgreSQLContainer postgresContainer = new PostgreSQLContainer("postgres:12.18")
+class ReviewRepositoryReuseTestContainersTest {
+  private static PostgreSQLContainer postgresContainer = (PostgreSQLContainer) new PostgreSQLContainer("postgres:12.18")
     .withDatabaseName("mytestdb")
     .withUsername("duke")
-    .withPassword("s3cret");
+    .withPassword("s3cret")
+    .withReuse(true);
 
   @DynamicPropertySource
   private static void propertyOverride(DynamicPropertyRegistry registry) {
     registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
     registry.add("spring.datasource.username", postgresContainer::getUsername);
     registry.add("spring.datasource.password", postgresContainer::getPassword);
+  }
+
+  static {
+    postgresContainer.start();
   }
 
   @Autowired private EntityManager entityManager;
