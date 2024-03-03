9d162a45a5d2badc5559e4a7b3c77c0fdc57b6db
Mike Smithson <mikesmithso@gmail.com>
2024-03-02 23:09:24 -0500

    add DataJPATest

--- src/test/java/de/rieckpil/courses/book/review/ReviewRepositoryTest.java
+++ src/test/java/de/rieckpil/courses/book/review/ReviewRepositoryTest.java
@@ -1,15 +1,22 @@
 package de.rieckpil.courses.book.review;
 
 import java.sql.SQLException;
+import java.time.LocalDateTime;
 
 import javax.sql.DataSource;
 
+import de.rieckpil.courses.book.management.Book;
+import de.rieckpil.courses.book.management.User;
 import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest(
     properties = {
@@ -23,15 +30,77 @@ class ReviewRepositoryTest {
 
   @Autowired private EntityManager entityManager;
 
-  @Autowired private ReviewRepository cut;
+  @Autowired private ReviewRepository reviewRepository;
 
   @Autowired private DataSource dataSource;
 
   @Autowired private TestEntityManager testEntityManager;
 
-  @Test
-  void notNull() throws SQLException {}
+  @BeforeEach
+  void setUp() {
+    assertThat(reviewRepository.count()).isZero();
+  }
 
   @Test
-  void transactionalSupportTest() {}
+  void notNull() throws SQLException {
+    assertThat(entityManager).isNotNull();
+    assertThat(reviewRepository).isNotNull();
+    assertThat(dataSource).isNotNull();
+    assertThat(testEntityManager).isNotNull();
+    System.out.println("using database: " + dataSource.getConnection().getMetaData().getDatabaseProductName());
+
+    Book java = new Book();
+    java.setTitle("Java Book");
+    java.setIsbn("0-9445-7984-1");
+    java.setAuthor("James Gosling");
+    java.setGenre("Computer Technology");
+    java.setDescription("A book on Java");
+    java.setPages(880L);
+    java.setPublisher("LeanPub");
+
+    User user = new User();
+    user.setCreatedAt(LocalDateTime.now());
+    user.setEmail("user@gmail.com");
+    user.setName("Buster Scruggs");
+
+    Review goodReview = new Review();
+    goodReview.setBook(java);
+    goodReview.setTitle("Review of Java Book");
+    goodReview.setContent("A great book for java developers at any skill level written by the founder of the language himself");
+    goodReview.setRating(5);
+    goodReview.setUser(user);
+    goodReview.setCreatedAt(LocalDateTime.now());
+
+    Review savedReview = testEntityManager.persist(goodReview);
+    assertThat(savedReview.getId()).isNotNull();
+  }
+
+  @Test
+  void transactionalSupportTest() {
+    Book java = new Book();
+    java.setTitle("Java Book");
+    java.setIsbn("0-9445-7984-1");
+    java.setAuthor("James Gosling");
+    java.setGenre("Computer Technology");
+    java.setDescription("A book on Java");
+    java.setPages(880L);
+    java.setPublisher("LeanPub");
+
+    User user = new User();
+    user.setCreatedAt(LocalDateTime.now());
+    user.setEmail("user@gmail.com");
+    user.setName("Buster Scruggs");
+
+    Review goodReview = new Review();
+    goodReview.setBook(java);
+    goodReview.setTitle("Review of Java Book");
+    goodReview.setContent("A great book for java developers at any skill level written by the founder of the language himself");
+    goodReview.setRating(5);
+    goodReview.setUser(user);
+    goodReview.setCreatedAt(LocalDateTime.now());
+
+    Review savedReview = reviewRepository.save(goodReview);
+    assertThat(savedReview.getId()).isNotNull();
+
+  }
 }
